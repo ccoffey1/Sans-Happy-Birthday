eee1134bf99e66713a741a44ac546a154361830e
Christian Coffey <Coffey@Christians-MacBook-Pro.local>
2018-03-01 13:00:22 -0500

    Uploading project to git.

---
+++ WindowsFormsApplication2/Form1.sanssays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1
+    {
+        void SansSays(string s, int speed, bool lastMessage, int endDelay, string sansType)
+        {
+            switch (sansType)
+            {
+                case "normal":
+                    sansBox.Image = Properties.Resources.sans_normal;
+                    break;
+                case "smirk":
+                    sansBox.Image = Properties.Resources.sans_smirk;
+                    break;
+                case "wink":
+                    sansBox.Image = Properties.Resources.sans_wink;
+                    break;
+            }
+        }
+}
