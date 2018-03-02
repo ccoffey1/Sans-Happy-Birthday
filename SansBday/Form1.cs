6f0d10c5f42ec97b4f294ccb00be2682cba9fd96
Christian Coffey <ccoffey@buffalo.edu>
2018-03-02 00:16:05 -0500

    reuploaded renamed program with executable in main directory.

---
+++ SansBday/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SansBday
+{
+    /**
+    * Garbage code made in a day for a friend for this birthday. 
+    * Don't judge this as my ability to program--it was coded without regard for
+    * cleanliness or optimization...
+    **/
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+
+            //Sans talking thread
+            Thread sansTalkingThread = new Thread(SansDialogue);
+            sansTalkingThread.Start();
+        }
+
+        void SansDialogue()
+        {
+            SoundPlayer sansTalks = new SoundPlayer(Properties.Resources.sans_talk);
+            sansTalks.Play();
+            SansSays("hey kid,\nhow are ya?", 40, false, 1000, "normal");
+            SansSays("listen i know\nyou might be\nbusy right\nnow...", 40, false, 1200, "normal");
+            SansSays("...summer\nvacation and\nall that...", 40, false, 1000, "smirk");
+            SansSays("but i got a...", 40, false, 1000, "normal");
+            SansSays("bone to\npick with you.", 40, true, 1000, "wink");
+            SansSays("that would be\nfunnier if you\nwere a\nskeleton.", 40, true, 1000, "wink");
+            SansSays("anyway sorry\ni'm late but...\n", 40, false, 1200, "normal");
+            SansSays("i hope you had\na good day.", 40, false, 1000, "smirk");
+            SansSays("well...", 40, false, 1000, "smirk");
+            SansSays("since it's your\nbirthday and \nwhat not.", 40, true, 2000, "smirk");
+            SansSays("oh here, i got\nyou a hat, too.", 40, false, 1200, "normal");
+
+            engieSurpriseBox.Width = 100;
+            engieSurpriseBox.Height = 146;
+            engieSurpriseBox.Location = new Point(546, 399);
+            engieSurpriseBox.Image = Properties.Resources.engiehappy;
+
+            SansSays("there we go.", 40, true, 1000, "smirk");
+            Thread.Sleep(3000);
+            engieSurpriseBox.Visible = false;
+            sansBox.Visible = false;
+            GUI.Image = Properties.Resources.idk;
+            Thread.Sleep(500);
+            Thread shakeThread = new Thread(ShakeWindow);
+            shakeThread.Start();
+            Thread.Sleep(3500);
+            sansTalks.Stop();
+            Application.Exit();
+            Environment.Exit(0);
+        }
+
+        void ShakeWindow()
+        {
+            Random random = new Random();
+            while (true)
+            {
+                Thread.Sleep(10);
+                int x = random.Next(1000);
+                int y = random.Next(1000);
+                Location = new Point(x, y);
+            }
+        }
+
+        void SansSays(string s, int speed, bool lastMessage, int endDelay, string sansType)
+        {
+            switch (sansType)
+            {
+                case "normal":
+                    sansBox.Image = Properties.Resources.sans_normal;
+                    break;
+                case "smirk":
+                    sansBox.Image = Properties.Resources.sans_smirk;
+                    break;
+                case "wink":
+                    sansBox.Image = Properties.Resources.sans_wink;
+                    break;
+            }
+
+            chatBubbleImage.Visible = true;
+            sansDialogueText.Visible = true;
+
+            string result = "";
+            for (int i = 0; i < s.Length; ++i)
+            {
+                result = result + ' ' + s.ElementAt(i);
+                sansDialogueText.Text = result;
+                Thread.Sleep(speed);
+            }
+            if (lastMessage)
+            {
+                Thread.Sleep(endDelay);
+                chatBubbleImage.Visible = false;
+                sansDialogueText.Visible = false;
+            }
+            Thread.Sleep(endDelay);
+        }
+        
+    }
+}
